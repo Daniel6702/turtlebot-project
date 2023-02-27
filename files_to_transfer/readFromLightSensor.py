7f966cf8a5f92452f8715b5cb334fc06ca3cae9d
Daniel6702 <pedersendaniel3561@gmail.com>
2023-02-27 10:52:08 +0100

    distance sensor and led main

--- files_to_transfer/readFromLightSensor.py
+++ files_to_transfer/readFromLightSensor.py
@@ -1,17 +1,32 @@
 import smbus
 import time
+
+class color_sensor():
+    def __init__(self):
+        self.bus = smbus.SMBus(1)
+        self.address = 0x44
+        self.bus.write_byte_data(self.address, 0x01, 0x05)
+
+    def getAndUpdateColour(self):
+        data = self.bus.read_i2c_block_data(self.address,0x09, 6)  
+        green = int((data[1] * 256 + data[0])/256)
+        red = int((data[3] * 256 + data[2])/256)
+        blue = int((data[5] * 256 + data[4])/256)
+        return (red,green,blue)
+    
+
 #testtsettest
 # Get I2C bus
-bus = smbus.SMBus(1)
+
 
 # ISL29125 address, 0x44(68)
-address = 0x44
+
 # Select configuation-1register, 0x01(01)
 # 0x0D(13) Operation: RGB, Range: 360 lux, Res: 16 Bits
-bus.write_byte_data(0x44, 0x01, 0x05)
+#bus.write_byte_data(0x44, 0x01, 0x05)
 
-time.sleep(1)
-
+#time.sleep(1)
+'''
 print("Reading colour values and displaying them in a new window\n")
 
 def getAndUpdateColour():
@@ -25,3 +40,4 @@ def getAndUpdateColour():
         time.sleep(2) 
 
 getAndUpdateColour()
+'''
