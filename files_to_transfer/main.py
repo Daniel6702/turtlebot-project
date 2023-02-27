f22be21d8eb22a7609da57c6db140ca37bb4a7f7
Daniel6702 <pedersendaniel3561@gmail.com>
2023-02-27 10:51:39 +0100

    distance sensor and led and main

--- files_to_transfer/main.py
+++ files_to_transfer/main.py
@@ -1,3 +1,13 @@
 import time
-import RPi.GPIO as GPIO
-import smbus
+import ultrasonic_sensor
+import readFromLightSensor
+
+def main():
+    distance_sensor = ultrasonic_sensor.Dist()
+    dist = distance_sensor.Measure(15)
+
+    color_sensor = readFromLightSensor.color_sensor()
+    color = color_sensor.getAndUpdateColour()
+
+if __name__ == "__main__":
+    main()
